a20b12e8833480388fffa808b8c9b64ebde421c0
李德龙 <7961918+li-delong@user.noreply.gitee.com>
2020-11-02 22:33:04 +0800

    部署

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import {Button } from 'antd';
+import 'antd/dist/antd.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary" >我是一个按钮</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
